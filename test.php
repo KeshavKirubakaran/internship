d81a7b35a342489b4336d60567e4b8f92a254882
Anand Surya <crazyanand.surya@gmail.com>
2018-03-09 22:41:15 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+$con = mysqli_connect("sql310.epizy.com","epiz_21741829","shuttle","epiz_21741829_saveenews");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
+
+
+
